5d645fd31b4e44f092a9330d18f9c177d37b4ac1
Anna Casper <anna.casper3@gmail.com>
2015-07-15 11:05:19 -0600

    user can add circles and squares to canvas

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -2,16 +2,31 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 canvas.addEventListener('click', function(){
-  position(50);
+  var shape = document.getElementById('shape').value
+  var color = document.getElementById('color').value
+  var width = document.getElementById('width').value
+  position(width, color, shape);
 });
 
-var position = function(width){
+var position = function(width, color, shape){
   var x = event.x;
   var y = event.y;
-  drawShape(x, y, width, width);
+  if(shape === 'Square'){
+    console.log('hello');
+    drawSquare(x, y, width, width, color);
+  } else if(shape === 'Circle'){
+    drawCircle(x, y, width, color);
+  }
 };
 
-var drawShape = function(x, y, width, width){
-  ctx.fillStyle = "rgb(200,0,0)";
+var drawSquare = function(x, y, width, width, color){
+  ctx.fillStyle = color;
   ctx.fillRect (x, y, width, width);
 };
+
+var drawCircle = function(x, y, width, color){
+  ctx.beginPath();
+  ctx.arc(x,y,width/2,0,2*Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
