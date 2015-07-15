52df324b161ed5269c0485a14e1374c4eae12cba
Anna Casper <anna.casper3@gmail.com>
2015-07-15 11:32:30 -0600

    object oriented

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var shapesArray = [];
 
 canvas.addEventListener('click', function(){
   var shape = document.getElementById('shape').value
@@ -8,25 +9,49 @@ canvas.addEventListener('click', function(){
   position(width, color, shape);
 });
 
-var position = function(width, color, shape){
-  var x = event.x;
-  var y = event.y;
-  if(shape === 'Square'){
-    console.log('hello');
-    drawSquare(x, y, width, width, color);
-  } else if(shape === 'Circle'){
-    drawCircle(x, y, width, color);
-  }
+function Shape(width, color, shape){
+  this.width = width,
+  this.color = color,
+  this.shape = shape
 };
 
-var drawSquare = function(x, y, width, width, color){
+function Square(width, color){
+  Shape.call(this, width, color, 'Square');
+};
+
+Square.prototype = new Shape();
+Square.prototype.constructor = Square;
+
+Square.prototype.drawSquare = function(x, y, width, width, color){
   ctx.fillStyle = color;
   ctx.fillRect (x, y, width, width);
 };
 
-var drawCircle = function(x, y, width, color){
+function Circle(width, color){
+  Shape.call(this, width, color, 'Circle');
+};
+
+Circle.prototype = new Shape();
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.drawCircle = function(x, y, width, color){
   ctx.beginPath();
   ctx.arc(x,y,width/2,0,2*Math.PI);
   ctx.fillStyle = color;
   ctx.fill();
-}
+};
+
+var position = function(width, color, shape){
+  var x = event.x;
+  var y = event.y;
+  if(shape === 'Square'){
+    var square = new Square(width, color);
+    shapesArray.push(square);
+    square.drawSquare(x, y, width, width, color);
+  } else if(shape === 'Circle'){
+    var circle = new Circle(width, color);
+    shapesArray.push(circle);
+    console.log(shapesArray);
+    circle.drawCircle(x, y, width, color);
+  }
+};
